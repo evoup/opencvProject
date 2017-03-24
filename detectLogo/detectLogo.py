b7a71e697e236406decf0c18d51c667728c32852
evoup <yinjia@madhouse-inc.com>
2017-03-24 18:02:59 +0800

    check contours

--- detectLogo/detectLogo.py
+++ detectLogo/detectLogo.py
@@ -2,26 +2,38 @@
 import cv2
 import numpy as np
 
-
+fileName = '../materials/appui/0319_12.png'
+fileName_grayed = '../materials/appui/0319_12_gray.png'
 # from matplotlib import pyplot as plt
-im_gray = cv2.imread('../materials/appui/0319_14.png', 0)
+img = cv2.imread(fileName, 0)
 
-height, width = im_gray.shape[:2]
-img = cv2.resize(im_gray, (402, 743), interpolation=cv2.INTER_LINEAR)
+def resize():
+    global img
+    height, width = img.shape[:2]
+    img = cv2.resize(img, (402, 743), interpolation=cv2.INTER_LINEAR)
+resize()
 
 _, img = cv2.threshold(img, 254, 255, cv2.THRESH_BINARY)
 cv2.imshow('img', img)
+cv2.waitKey(0)
 
-
-image, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+######
+cv2.imwrite(fileName_grayed, img)
+oldImg = img.copy()
+img = cv2.imread(fileName_grayed, 1)
+resize()
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(imgray, 250, 255, 0)
+image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
     rect = cv2.minAreaRect(cnt)
     box = cv2.boxPoints(rect)
     box = np.int0(box)
     cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
-
+######
+cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
