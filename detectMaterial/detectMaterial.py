1488d2d13c10a5d8dfd1b7751737c26ea1602f9f
evoup <evoex123@gmail.com>
2017-03-25 19:45:33 +0800

    clear code

--- detectMaterial/detectMaterial.py
+++ detectMaterial/detectMaterial.py
@@ -42,14 +42,6 @@ for cnt in contours:
         #break
 
 
-# for c in contours:
-#     peri = cv2.arcLength(c, True)
-#     approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-#     if len(approx) == 4:
-#         ctaCnt = approx
-#         res = cv2.drawContours(img, [ctaCnt], 0, (0, 255, 0), 2)
-#         break
-######
 cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -58,16 +50,3 @@ cv2.destroyAllWindows()
 
 
 
-# height, width = img.shape[:2]
-# img = cv2.resize(img, (402, 743), interpolation=cv2.INTER_LINEAR)
-# imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# ret, thresh = cv2.threshold(imgray, 250, 255, 0)
-# image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# for cnt in contours:
-#     x, y, w, h = cv2.boundingRect(cnt)
-#     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-#     rect = cv2.minAreaRect(cnt)
-#     box = cv2.boxPoints(rect)
-#     box = np.int0(box)
-#     cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
-# cv2.imshow('img', img)
