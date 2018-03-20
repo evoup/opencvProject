58a99f885e8c76f568e4a16efb3e639c842a7443
evoup <evoex123@gmail.com>
2018-03-20 12:49:29 +0800

    add debugmode flag

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ GRAB_MOBILE_WEB = False
 # ADB_SERIAL = "-s 127.0.0.1:62001"
 ADB_SERIAL = "-s 127.0.0.1:62001"
 SAVE_DIR = os.path.join("D:/", "grabFbAd")
-DEBUG = True
+DEBUG = False
 
 # get arguments
 parser = OptionParser(description='Manages collectors which gather '
@@ -32,6 +32,10 @@ parser.add_option('-x', '--snapshot_file_suffix', dest='snapshot_file_suffix', m
                   help='snapshot file suffix, different from every collector process. e.g.: _1 should be snapshot_1.png '
                        'in the end')
 
+parser.add_option('-d', '--debug', dest='debugmode', metavar='DEBUGMODE',
+                  default="",
+                  help='debug mode, default is not debug mode, if set with 1, then will be in debug mode')
+
 (options, args) = parser.parse_args(args=argv[1:])
 
 if options.adb_serial_number:
@@ -40,4 +44,6 @@ if options.adb_serial_number:
 if options.snapshot_file_suffix:
     FILE_NAME = "ad_screenshot" + options.snapshot_file_suffix + ".png"
 
+if options.debugmode:
+    DEBUG = True
 print "test"
